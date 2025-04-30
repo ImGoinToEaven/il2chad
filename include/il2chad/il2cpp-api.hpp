67f561df2b4bdba617d479320c528c4792316203
ImGoinToEaven <155204866+ImGoinToEaven@users.noreply.github.com>
2025-04-30 22:49:08 +0000

    more api

--- include/il2chad/il2cpp-api.hpp
+++ include/il2chad/il2cpp-api.hpp
@@ -11,18 +11,23 @@ namespace il2chad::il2cpp {
     IL2CPP_API(Il2CppAssembly *, il2cpp_domain_assembly_open, Il2CppDomain *domain, const char *name)
     IL2CPP_API(Il2CppImage *, il2cpp_assembly_get_image, Il2CppAssembly const *assembly)
 
+
+    IL2CPP_API(size_t, il2cpp_image_get_class_count, const Il2CppImage *image);
+    IL2CPP_API(const Il2CppClass *, il2cpp_image_get_class, const Il2CppImage *image, size_t index);
+
     IL2CPP_API(Il2CppClass *, il2cpp_class_from_name, Il2CppImage const *image, const char *namespace_name,
                const char *class_name)
     IL2CPP_API(MethodInfo const *, il2cpp_class_get_methods, Il2CppClass *klass, void **iter)
     IL2CPP_API(FieldInfo const *, il2cpp_class_get_fields, Il2CppClass *klass, void **iter)
     IL2CPP_API(FieldInfo const *, il2cpp_class_get_field_from_name, Il2CppClass *klass, const char *name);
-    IL2CPP_API(const MethodInfo*, il2cpp_class_get_method_from_name, Il2CppClass * klass, const char* name, int argsCount);
-    IL2CPP_API(const char*, il2cpp_class_get_name, Il2CppClass * klass);
-    IL2CPP_API(const char*, il2cpp_class_get_namespace, Il2CppClass * klass);
+    IL2CPP_API(const MethodInfo *, il2cpp_class_get_method_from_name, Il2CppClass *klass, const char *name,
+               int argsCount);
+    IL2CPP_API(const char *, il2cpp_class_get_name, Il2CppClass *klass);
+    IL2CPP_API(const char *, il2cpp_class_get_namespace, Il2CppClass *klass);
 
     IL2CPP_API(Il2CppClass *, il2cpp_object_get_class, Il2CppObject *obj);
     IL2CPP_API(Il2CppObject *, il2cpp_object_new, Il2CppClass *klass);
-    IL2CPP_API(void, il2cpp_runtime_object_init, Il2CppObject * obj);
+    IL2CPP_API(void, il2cpp_runtime_object_init, Il2CppObject *obj);
 
     IL2CPP_API(char const *, il2cpp_method_get_name, MethodInfo const *method)
     IL2CPP_API(int, il2cpp_method_get_param_count, const MethodInfo *method)
