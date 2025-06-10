09f8f46025d7ffa1c54b23ab68a1cc7cbeb4b4b6
ImGoinToEaven <155204866+ImGoinToEaven@users.noreply.github.com>
2025-06-10 23:58:27 +0000

    unbox util for strings

--- src/il2chad/il2cpp-structs.cpp
+++ src/il2chad/il2cpp-structs.cpp
@@ -2,12 +2,11 @@
 #include <string>
 #include "il2chad/il2cpp-api.hpp"
 
-const char *il2chad::il2cpp::Il2CppString::Unbox() {
-    thread_local std::string buffer;
-    buffer.clear();
+std::string il2chad::il2cpp::Il2CppString::Unbox() {
+    std::string buffer;
     auto *chars = il2cpp_string_chars(this);
     auto length = il2cpp_string_length(this);
     for (int i = 0; i < length; ++i)
         buffer += static_cast<char>(chars[i]);
-    return buffer.c_str();
+    return buffer;
 }
